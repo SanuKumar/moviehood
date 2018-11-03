56f6ec0cf0dcbae71aaa1d579e955886ef95a689
Sanu Kumar <sanukumar1138@Gmail.com>
2018-11-03 17:44:49 +0530

    using component with other component

---
+++ src/app/first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector:'via-first',
+    template: '<h2> My First Component</h2>'
+})
+
+export class FirstComponent {
+    title= "app works!!"
+}
